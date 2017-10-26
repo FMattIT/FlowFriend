f6c96ecb7d7c622b4a992610fd2a66a1f8ffaa8c
Admin <maciej.furman.official@gmail.com>
2017-10-26 21:24:57 +0200

    v.0.8 - Working on new, refactored and clean code.

--- flow/src/main/resources/static/js/calendar/calendar.js
+++ flow/src/main/resources/static/js/calendar/calendar.js
@@ -2,7 +2,13 @@
  * Created by Admin on 23.10.2017.
  */
 
-var calendarInstance = new Calendar(1, 1, 2017, 0);
+var calendarInstance = new Calendar(1, 9, 2017);
+//10 - listopad
+//11- listopad
+
+$( document ).ready(function() {
+    calendarInstance.generateCalendar();
+});
 
 function Calendar(currentGoalId, currentMonthId, currentYear) {
     this.months = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'];
@@ -18,10 +24,10 @@ function Calendar(currentGoalId, currentMonthId, currentYear) {
     this.currentYear = currentYear;
     this.daysInMonth = this.daysInMonths[this.currentMonthId];
 
-    var daysss = days_in_month[this_month_id];
-    if((this_year % 4 == 0 &&this_year % 100 != 0) || this_year % 400 == 0){
-        daysss = 29;
+    if((this.currentYear % 4 == 0 &&this.currentYear % 100 != 0) || this.currentYear % 400 == 0){
+        this.daysInMonth = 29;
     }
+
 
     this.goals;
     this.goalPosition;
@@ -31,67 +37,68 @@ Calendar.prototype.init = function() {}
 
 Calendar.prototype.generateCalendar = function() {
 
-    var day = 1;
-    var rows;
-    var days;
+    let currentlyCreatingDay = 1;
+    let tableRowNumber;
+    let dayInRowNumber; //days
+    let table = $(".calendar__days__table");
+    let today = new Date();
 
-    var tr = document.createElement('tr');
-
-    for(rows=1; rows<=6; rows++) {
+    for(tableRowNumber=1; tableRowNumber<=6; tableRowNumber++) {
         var tr = document.createElement('tr');
-        for(days=1; days <=7; days++){
-            if(rows==1 && days<=new Date(this_year, this_month_id, 1).getDay()){
-                if(days==new Date(this_year, this_month_id, 1).getDay()){
-                    day=1;
+        tr.className = "calendar__days__table__row"
+        for(dayInRowNumber=1; dayInRowNumber <=7; dayInRowNumber++){
+            if(tableRowNumber==1 && dayInRowNumber<=new Date(this.currentYear, this.currentMonthId, 1).getDay()){
+                if(dayInRowNumber==new Date(this.currentYear, this.currentMonthId, 1).getDay()){
+                    currentlyCreatingDay=1;
                 }
                 else{
                     var td = document.createElement('td');
-                    td.innerHTML = new Date(this_year, this_month_id, 0).getDate()+days-new Date(this_year, this_month_id, 1).getDay()+1;
-                    td.className = "day empty";
+                    td.innerHTML = new Date(this.currentYear, this.currentMonthId, 0).getDate()+dayInRowNumber-new Date(this.currentYear, this.currentMonthId, 1).getDay()+1;
+                    td.className = "day_cell";
                     tr.appendChild(td);
-                    table.appendChild(tr);
-                    day++
+                    table.append(tr);
+                    currentlyCreatingDay++
                     continue;
                 }
             }
 
-            if(day>daysss && day<42){
+            if(currentlyCreatingDay>this.daysInMonth && currentlyCreatingDay<42){
                 var td = document.createElement('td');
-                td.innerHTML = new Date(this_year, this_month_id+1, 1).getDate()+day-daysss-1;
-                td.className = "day empty";
+                td.innerHTML = new Date(this.currentYear, this.currentMonthId+1, 1).getDate()+currentlyCreatingDay-this.daysInMonth-1;
+                td.className = "day_cell";
             }
-            else if(day>daysss){break;}
+            else if(currentlyCreatingDay>this.daysInMonth){break;}
             else{
                 var td = document.createElement('td');
-                td.innerHTML = day;
+                td.innerHTML = currentlyCreatingDay;
 
-                if((day==new Date().getUTCDate() || day==new Date().getUTCDate()-1 || day==new Date().getUTCDate()-2) && this_month_id==new Date().getMonth()){td.className = "day"; td.setAttribute('onclick', 'onDayClick(this)');}else {td.className = "day disabled";}
+                if((currentlyCreatingDay==new Date().getUTCDate() || currentlyCreatingDay==new Date().getUTCDate()-1 || currentlyCreatingDay==new Date().getUTCDate()-2) && this.currentMonthId==new Date().getMonth()){td.className = "day_cell"; td.setAttribute('onclick', 'onDayClick(this)');}else {td.className = "day_cell";}
 
-                if((new Date().getDate()==1 && this_month_id == today.getMonth()-1 && (td.innerHTML==31 || td.innerHTML==30)) || (new Date().getDate()==2 && this_month_id == today.getMonth()-1 && (td.innerHTML==31)))
+                if((new Date().getDate()==1 && this.currentMonthId == today.getMonth()-1 && (td.innerHTML==31 || td.innerHTML==30)) || (new Date().getDate()==2 && this.currentMonthId == today.getMonth()-1 && (td.innerHTML==31)))
                 {
-                    td.className = "day";
+                    td.className = "day_cell";
                     td.setAttribute('onclick', 'onDayClick(this)');
                 }
 
-                for(var i=0; i<value[0].length; i++){
-                    var t= value[1][Number(actual_goal_id.innerHTML)];
-                    if(value[0][i].goalId.id == t.id && value[0][i].month == this_month_id && value[0][i].day == day && value[0][i].flag == "TICK") {
-                        td.style.cssText = "background-color: #009966; background-size: cover; border: 0; color:white;";
-                    }
-                    else if (value[0][i].goalId.id == value[1][Number(actual_goal_id.innerHTML)].id && value[0][i].month == this_month_id && value[0][i].day == day && value[0][i].flag == "CROSS") {
-                        td.style.cssText = "background-color: #e74c3c; background-size: cover; border: 0; color:white;";
-                    }
-                    else if (value[0][i].goalId.id == value[1][Number(actual_goal_id.innerHTML)].id && value[0][i].month == this_month_id && value[0][i].day == day && value[0][i].flag == "YELLOWTICK") {
-                        td.style.cssText = "background-color: #f1c40f; background-size: cover; border: 0; color:white;";
-                    }
-                    else if (value[0][i].goalId.id == value[1][Number(actual_goal_id.innerHTML)].id && value[0][i].month == this_month_id && value[0][i].day == day && value[0][i].flag == "MINUS") {
-                        td.style.cssText = "background-color: #7f8c8d; background-size: cover; border: 0; color:white;";
-                    }
-                }
+                // for(var i=0; i<value[0].length; i++){
+                //     var t= value[1][Number(actual_goal_id.innerHTML)];
+                //     if(value[0][i].goalId.id == t.id && value[0][i].month == this.currentMonthId && value[0][i].day == currentlyCreatingDay && value[0][i].flag == "TICK") {
+                //         td.style.cssText = "background-color: #009966; background-size: cover; border: 0; color:white;";
+                //     }
+                //     else if (value[0][i].goalId.id == value[1][Number(actual_goal_id.innerHTML)].id && value[0][i].month == this.currentMonthId && value[0][i].day == currentlyCreatingDay && value[0][i].flag == "CROSS") {
+                //         td.style.cssText = "background-color: #e74c3c; background-size: cover; border: 0; color:white;";
+                //     }
+                //     else if (value[0][i].goalId.id == value[1][Number(actual_goal_id.innerHTML)].id && value[0][i].month == this.currentMonthId && value[0][i].day == currentlyCreatingDay && value[0][i].flag == "YELLOWTICK") {
+                //         td.style.cssText = "background-color: #f1c40f; background-size: cover; border: 0; color:white;";
+                //     }
+                //     else if (value[0][i].goalId.id == value[1][Number(actual_goal_id.innerHTML)].id && value[0][i].month == this.currentMonthId && value[0][i].day == currentlyCreatingDay && value[0][i].flag == "MINUS") {
+                //         td.style.cssText = "background-color: #7f8c8d; background-size: cover; border: 0; color:white;";
+                //     }
+                // }
             }
             tr.appendChild(td);
-            table.appendChild(tr);
-            day++
+            table.append(tr);
+            currentlyCreatingDay++
         }
     }
 }
